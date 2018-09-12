27d2df1e54cf2ba0403982a40fd6a0b93ccd6b65
urankajtazaj <ukajtazi@gmail.com>
2018-09-12 12:43:11 +0200

    initial commit

---
+++ js/slider.js
@@ -0,0 +1,39 @@
+
+var container = $(".slider-container");
+var contWidth = $(container)[0].getBoundingClientRect().width;
+var next = $("#next");
+var prev = $("#prev");
+var currentIndex = 0;
+
+var imageName = "images/img-";
+var imageExtension = ".jpeg";
+
+function nextImage() {
+    if (currentIndex >= $(container).find("img").length - 1) {
+        currentIndex = -1;
+    }
+    
+    container.animate({
+        scrollLeft: contWidth * ++currentIndex
+    });
+}
+
+function prevImage() {
+    if (currentIndex <= 0) {
+        currentIndex = $(container).find("img").length;
+    }
+
+    container.animate({
+        scrollLeft: contWidth * --currentIndex
+    });
+}
+
+function addImages(start, end) {
+    for (var i = start; i <= end; i++) {
+        $(container).append("<img src='" + imageName + i + imageExtension + "'/>");
+    }
+}
+
+$(document).ready(function() {
+    addImages(1, 6);
+});
