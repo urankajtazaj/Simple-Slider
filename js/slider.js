c5efaee8093632eaae82b60cf893ad72ad5d11ca
urankajtazaj <ukajtazi@gmail.com>
2018-09-14 10:49:41 +0200

    Converted to library

--- js/slider.js
+++ js/slider.js
@@ -1,206 +1,243 @@
+/* 
+ * Created by Uran Kajtazaj 
+ */
 
-// USER CONFIG
-var contHeight = 800;
-var sliderWidth = "100%";
-var slideSpeed = 200;
-var showDots = true;
+function SimpleSlider() {
 
-var images = [
-     "images/img-1.jpeg"
-    ,"images/img-2.jpeg"
-    ,"images/img-3.jpeg"
-    ,"images/img-4.jpeg"
-    ,"images/img-5.jpeg"
-    ,"images/img-6.jpeg"
-];
+    var self = this;
 
+    this.DotsVisible = true;
+    this.Auto = true;
+    this.PauseSlideOnMouseEntered = true;
+    this.ArrowsVisible = true;
 
-var sliderContainer = $(".slider");
-var container = $(".slider-container");
-var contWidth = $(container)[0].getBoundingClientRect().width;
-var currentIndex = 0;
+    this.ContHeight = 800;
+    this.SliderWidth = "100%";
+    this.SlideSpeed = 500;
+    this.AutoSlideTime = 5000;
+    this.next = $("#next");
+    this.prev = $("#prev");
+    this.images = [];
 
-var imageName = "images/img-";
-var imageExtension = ".jpeg";
-
-$(next).on('click', function() {
-    nextImage();
-})
-
-$(prev).on('click', function() {
-    prevImage();
-})
-
-function sliderDots() {
-    sliderContainer.append("<div class='slider-dots'></div>");
-    for (var i = 0; i < images.length; i++) {
-        sliderContainer.find(".slider-dots").append("<span class='dot' onclick='slideToIndex(" + i + ")'></span>");
-    }
-
-    $(sliderContainer.find(".dot")[0]).addClass("current");
-
-}
-
-function slideToIndex(index) {
-    container.animate({
-        scrollLeft: contWidth * index
-    }, slideSpeed);
-
-    changeDotLocation(index);
-   
-}
-
-function changeDotLocation(index) {
-    $(".dot").css({
-        "background-color": "rgba(255, 255, 255, .7)"
-       ,"transform": "translateY(0)"
-   });
-
-   $($(".dot")[index]).css({
-        "background-color": "#fff"
-       ,"transform": "translateY(-4px)"
-   });
-}
-
-function nextImage() {
-    if (currentIndex >= $(container).find("img").length - 1) {
-        currentIndex = -1;
-    }
-    
-    container.animate({
-        scrollLeft: contWidth * ++currentIndex
-    }, slideSpeed);
-
-    changeDotLocation(currentIndex);
-}
-
-function prevImage() {
-    if (currentIndex <= 0) {
-        currentIndex = $(container).find("img").length;
-    }
-
-    container.animate({
-        scrollLeft: contWidth * --currentIndex
-    }, slideSpeed);
-
-    changeDotLocation(currentIndex);
-}
-
-function addImages() {
-    images.forEach(imagePath => {
-        $(container).append("<img src='" + imagePath + "'/>");        
-    });
-}
-
-function addStyles() {
-    sliderContainer.css({
-         "width": sliderWidth
-        ,"position": "relative"
-    });
-
-    $(".slider-dots").css({
-         "text-align": "center"
-        ,"position": "absolute"
-        ,"width": "100%"
-    });
-
-    $(".dot").css({
-         "margin": "-25px 4px 0 4px"
-        ,"display": "inline-block"
-        ,"height": "15px"
-        ,"width": "15px"
-        ,"background-color": "rgba(255, 255, 255, .7)"
-        ,"box-shadow": "0 0 10px rgba(0, 0, 0, 0.5)"
-        ,"border-radius": "20px"
-        ,"z-index": "3"
-        ,"cursor": "pointer"
-        ,"transition": "150ms ease-in-out"
-    });
-
-    $(".dot.current").css({
-         "background-color": "#fff"
-        ,"transform": "translateY(-4px)"
-    });
-
-    $(".container").css({
-         "position": "relative"
-        ,"display": "flex"
-        ,"justify-content": "space-between"
-    });
+    this.sliderContainer = $(".slider");
+    this.container = $(".slider-container");
+    this.contWidth = $(self.container)[0].getBoundingClientRect().width;
+    this.currentIndex = 0;
 
     $(".arrow").css({
-        "background-color": "rgba(0, 0, 0, 0.2)"
-        ,"position": "absolute"
-        ,"display": "inline-block"
-        ,"height": "40px"
-        ,"padding": "10px"
-        ,"margin-top": "calc(" + contHeight + "px / 2 - 40px)"
-        ,"display": "flex"
-        ,"justify-content": "center"
-        ,"align-items": "center"
+        "display": "none"
     });
 
-    $(".arrow img").css({
-        "height": "30px"
-    });
+    self.nextImage = function() {
+        if (self.currentIndex >= $(self.container).find("img").length - 1) {
+            self.currentIndex = -1;
+        }
+        
+        $(self.container).animate({
+            scrollLeft: self.contWidth * ++self.currentIndex
+        }, self.SlideSpeed);
 
-    $(".arrow.left").css({
-         "left": "0"
-    });
-
-    $(".arrow.right").css({
-        "right": "0"
-    });
-    
-    $(".arrow.left").mouseenter(function() {
-        $(this).animate({
-            "padding-left": "5px"
-            ,"padding-right": "15px"
-        }, 150);
-    })
-
-    $(".arrow.left").mouseleave(function() {
-        $(this).animate({
-            "padding-left": "10px"
-            ,"padding-right": "10px"
-        }, 150);
-    })
-    
-    $(".arrow.right").mouseenter(function() {
-        $(this).animate({
-            "padding-right": "5px"
-            ,"padding-left": "15px"
-        }, 150);
-    })
-
-    $(".arrow.right").mouseleave(function() {
-        $(this).animate({
-            "padding-right": "10px"
-            ,"padding-left": "10px"
-        }, 150);
-    })
-
-    container.css({
-         "height": contHeight + "px"
-        ,"width": "100%"
-        ,"margin": "auto"
-        ,"display": "flex"
-        ,"justify-content": "flex-start"
-        ,"align-items": "center"
-        ,"overflow": "hidden"
-    });
-
-    $(container).find("img").css({
-        "min-width": "100%"
-    });
-
-}
-
-$(document).ready(function() {
-    addImages();
-    if (showDots) {
-        sliderDots();
+        self.changeDotLocation(self.currentIndex);
     }
 
-    addStyles();
-});
+    this.prevImage = function() {
+        if (self.currentIndex <= 0) {
+            self.currentIndex = $(self.container).find("img").length;
+        }
+
+       $(self.container).animate({
+            scrollLeft: self.contWidth * --self.currentIndex
+        }, self.SlideSpeed);
+
+        self.changeDotLocation(this.currentIndex);
+    }
+    
+    this.slideToIndex = function(index) {
+        $(self.container).animate({
+             scrollLeft: self.contWidth * index
+         }, self.SlideSpeed);
+    
+         self.changeDotLocation(index);
+         self.currentIndex = index;
+     }
+
+    this.sliderDots = function() {
+        self.sliderContainer.append("<div class='slider-dots'></div>");
+        for (var i = 0; i < self.images.length; i++) {
+            self.sliderContainer.find(".slider-dots").append("<span class='dot' data-index='" + i + "'></span>");
+        }
+
+        $(".dot").each(function(i, el) {
+            var el = $(el);
+            el.click(function() {
+                var id = el.attr("data-index");
+                self.slideToIndex(id);
+                $(self.sliderContainer.find(".dot")[0]).addClass("current");
+            });
+        });
+        $(self.sliderContainer.find(".dot")[0]).addClass("current");
+    }
+
+    this.changeDotLocation = function(index) {
+        $(".dot").css({
+            "background-color": "rgba(255, 255, 255, .7)"
+            ,"box-shadow": "0 0 0 2px rgba(0, 0, 0, 0.3)"
+        });
+
+        $($(".dot")[index]).css({
+            "background-color": "#fff"
+            ,"box-shadow": "0 0 0 3px rgba(0, 0, 0, 0.5)"
+        });
+    }
+
+    $(this.next).on('click', function() {
+        self.nextImage();
+    });
+
+    $(this.prev).on('click', function() {
+        self.prevImage();
+    });
+
+    this.addImages = function() {
+        self.images.forEach(imagePath => {
+            $(self.container).append("<img src='" + imagePath + "'/>");        
+        });
+    }
+
+    this.addStyles = function() {
+        self.sliderContainer.css({
+            "width": self.SliderWidth
+            ,"position": "relative"
+        });
+
+        $(".slider-dots").css({
+            "text-align": "center"
+            ,"position": "absolute"
+            ,"width": "100%"
+        });
+
+        $(".dot").css({
+            "margin": "-25px 5px 0 5px"
+            ,"display": "inline-block"
+            ,"height": "12px"
+            ,"width": "12px"
+            ,"background-color": "rgba(255, 255, 255, .7)"
+            ,"border-radius": "20px"
+            ,"z-index": "3"
+            ,"cursor": "pointer"
+            ,"transition": "150ms ease-in-out"
+        });
+
+        $(".dot.current").css({
+            "background-color": "#fff"
+            ,"box-shadow": "0 0 0 3px rgba(0, 0, 0, 0.5)"
+        });
+
+        $(".container").css({
+            "position": "relative"
+            ,"display": "flex"
+            ,"justify-content": "space-between"
+        });
+
+        $(".arrow").css({
+            "position": "absolute"
+            ,"display": "inline-block"
+            ,"height": "40px"
+            ,"padding": "10px"
+            ,"margin-top": "calc(" + self.ContHeight + "px / 2 - 40px)"
+            ,"display": "flex"
+            ,"justify-content": "center"
+            ,"align-items": "center"
+            ,"cursor": "pointer"
+        });
+
+        $(".arrow img").css({
+            "height": "35px"
+        });
+
+        $(".arrow.left").css({
+            "left": "0"
+        });
+
+        $(".arrow.right").css({
+            "right": "0"
+        });
+        
+        $(".arrow.left").mouseenter(function() {
+            $(this).animate({
+                "padding-left": "5px"
+                ,"padding-right": "15px"
+            }, 150);
+        })
+
+        $(".arrow.left").mouseleave(function() {
+            $(this).animate({
+                "padding-left": "10px"
+                ,"padding-right": "10px"
+            }, 150);
+        })
+        
+        $(".arrow.right").mouseenter(function() {
+            $(this).animate({
+                "padding-right": "5px"
+                ,"padding-left": "15px"
+            }, 150);
+        })
+
+        $(".arrow.right").mouseleave(function() {
+            $(this).animate({
+                "padding-right": "10px"
+                ,"padding-left": "10px"
+            }, 150);
+        })
+
+       this.container.css({
+            "height": self.ContHeight + "px"
+            ,"width": "100%"
+            ,"margin": "auto"
+            ,"display": "flex"
+            ,"justify-content": "flex-start"
+            ,"align-items": "center"
+            ,"overflow": "hidden"
+        });
+
+        $(this.container).find("img").css({
+            "min-width": "100%"
+        });
+    }
+
+    this.Show = function() {
+        self.addImages();
+
+        if (self.images.length > 0) {
+            if (self.DotsVisible && self.images.length > 1) {
+                self.sliderDots();
+            }
+        
+            self.addStyles();
+
+            if (self.ArrowsVisible) {
+                $(".arrow").css({
+                    "display": "normal"
+                });
+            }
+
+            if (self.Auto) {
+                this.slideInterval = setInterval(self.nextImage, self.AutoSlideTime);
+            }
+
+            if (self.Auto && self.PauseSlideOnMouseEntered) {
+                self.sliderContainer.mouseenter(function() {
+                    clearInterval(self.slideInterval);
+                });
+        
+                self.sliderContainer.mouseleave(function() {
+                    self.slideInterval = setInterval(self.nextImage, self.AutoSlideTime);
+                });
+            }
+        } else {
+            alert("Add images to the slider to continue");
+        }
+
+    };
+}
